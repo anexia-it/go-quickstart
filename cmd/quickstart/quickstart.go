e6faadeffb21482cf58ce2087d074a8a8ff5c6ab
Stephan Peijnik-Steinwender <speijnik@anexia-it.com>
2018-05-08 14:35:05 +0200

    Add cobra and root command

--- cmd/quickstart/quickstart.go
+++ cmd/quickstart/quickstart.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-  "fmt"
-  "github.com/anexia-it/go-quickstart"
-)
-
 func main() {
-  fmt.Printf("quickstart v%s\n", quickstart.VersionString())
+	cmdRoot.Execute()
 }
---
+++ cmd/quickstart/root.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var cmdRoot = &cobra.Command{
+	Use:   "quickstart",
+	Short: "Quickstart into Go sample application",
+}
