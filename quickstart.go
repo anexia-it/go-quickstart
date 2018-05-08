5ff3676f39bc63e843698d90adab4419ca5e459f
Stephan Peijnik-Steinwender <speijnik@anexia-it.com>
2018-05-08 14:33:38 +0200

    Run "go fmt"

--- quickstart.go
+++ quickstart.go
@@ -1,15 +1,16 @@
 package quickstart
 
 import (
-  "fmt"
+	"fmt"
 )
 
 // VersionMajor defines the major version number
 const VersionMajor int = 0
+
 // VersionMinor defines the minor version number
 const VersionMinor int = 9
 
 // VersionString returns the full version string
 func VersionString() string {
-  return fmt.Sprintf("%d.%d", VersionMajor, VersionMinor)
+	return fmt.Sprintf("%d.%d", VersionMajor, VersionMinor)
 }
