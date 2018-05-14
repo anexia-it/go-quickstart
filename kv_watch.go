7503d0698f842431ff8faa1be03073b30bc72c31
Stephan Peijnik-Steinwender <speijnik@anexia-it.com>
2018-05-14 13:23:21 +0200

    Implement watch logic

---
+++ kv_watch.go
@@ -0,0 +1,77 @@
+package quickstart
+
+import (
+	"context"
+	"github.com/hashicorp/consul/api"
+)
+
+type WatchResult struct {
+	Pair  *api.KVPair
+	Error error
+}
+
+func (k KV) Watch(ctx context.Context, prefix string) (res chan *WatchResult) {
+	res = make(chan *WatchResult, 128)
+
+	go func() {
+		defer close(res)
+
+		currentIndex := uint64(0)
+
+		for {
+			// This loop is supposed to run until either the context is canceled or the
+			// consul API reports an error
+			opts := &api.QueryOption{
+				WaitIndex: currentIndex,
+				WaitTime:  time.Minute,
+			}
+
+			// Retrieve list of changes keys
+			kvPairs, meta, err := k.consulKV.List(prefix, opts.WithContext(ctx))
+
+			// Error may be nested in url.Error
+			if err != nil {
+				if urlErr, isUrlErr := err.(*url.Error); isUrlErr {
+					err = urlErr.Error
+				}
+			}
+
+			if err == context.Canceled {
+				// Watching canceled, return without reporting an error
+				return
+			}
+
+			if err != nil {
+				// Report error and stop watching
+				result <- &WatchResult{
+					Error: err,
+				}
+				return
+			}
+
+			if currentIndex == meta.LastIndex {
+				// No changes, move on
+				continue
+			}
+
+			// Iterate over all changed KV pairs
+			for _, kvPair := range kvPairs {
+				if kvPair.CreateIndex <= currentIndex && kvPair.ModifyIndex <= currentIndex {
+					// Key was unmodified, but child key might have changed
+					continue
+				}
+
+				// Write changed paiur to channel
+				result <- &WatchResult{
+					Pair: kvPair,
+				}
+			}
+
+			// Update currentIndex for the next iteration of this loop
+			currentIndex = meta.LastIndex
+
+		}
+	}()
+
+	return
+}
