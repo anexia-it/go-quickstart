4815446a91d1df1f13d76c130a25662b27ad514a
Stephan Peijnik-Steinwender <speijnik@anexia-it.com>
2018-05-14 09:43:12 +0200

    Implement parsing of our request JSON structure

--- cmd/quickstart/parse_request.go
+++ cmd/quickstart/parse_request.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 
-	"github.com/anexia-it/go-quickstart"
+	"github.com/anexia-it/go-human"
 	"github.com/spf13/cobra"
+
+	"github.com/anexia-it/go-quickstart"
 )
+
+// encoder is implemented by json.Encoder and human.Encoder
+type encoder interface {
+	Encode(o interface{}) error
+}
 
 var cmdParseRequest = &cobra.Command{
 	Use:   "parse-request <path>",
 	Short: "Parses a request",
 	RunE: func(cmd *cobra.Command, args []string) (err error) {
-		logger := quickstart.GetRootLogger().Named("parse_request")
 		if len(args) != 1 {
 			return fmt.Errorf("usage: %s", cmd.Use)
+		}
+
+		// Check if we want JSON output
+		wantJSON, _ := cmd.Flags().GetBool("json")
+
+		var enc encoder
+		if wantJSON {
+			jsonEncoder := json.NewEncoder(cmd.OutOrStdout())
+			jsonEncoder.SetIndent("", "  ")
+			enc = jsonEncoder
+		} else {
+			enc, _ = human.NewEncoder(cmd.OutOrStdout())
 		}
 
 		// Try to open the file
@@ -30,12 +49,13 @@ var cmdParseRequest = &cobra.Command{
 			return
 		}
 
-		logger.Sugar().Infof("Request: %s\n", r)
+		err = enc.Encode(r)
 
 		return
 	},
 }
 
 func init() {
+	cmdParseRequest.Flags().BoolP("json", "j", false, "JSON output")
 	cmdRoot.AddCommand(cmdParseRequest)
 }
