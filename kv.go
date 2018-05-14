8ee7cae137468b4e126a4cc5f084d8971b42b4ea
Stephan Peijnik-Steinwender <speijnik@anexia-it.com>
2018-05-14 10:45:20 +0200

    Add basic KV operations

--- kv.go
+++ kv.go
@@ -1,26 +1,54 @@
 package quickstart
 
 import (
-  "github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/api"
 )
 
 type KV struct {
-  consulClient *api.Client
+	consulKV *api.KV
+}
+
+// Get retrieves a KV key
+func (k KV) Get(key string) (data []byte, err error) {
+	var kvPair *api.KVPair
+	if kvPair, _, err = k.consulKV.Get(key, nil); err != nil {
+		return
+	}
+
+	data = kvPair.Value
+	return
+}
+
+// Put writes a key to the consul KV
+func (k KV) Put(key string, data []byte) (err error) {
+	kvPair := &api.KVPair{
+		Key:   key,
+		Value: data,
+	}
+
+	_, err = k.consulKV.Put(kvPair, nil)
+	return
+}
+
+// Delete removes a key from consul KV
+func (k KV) Delete(key string) (err error) {
+	_, err = k.consulKV.Delete(key, nil)
+	return
 }
 
 // NewKV constructs new KV connection
 func NewKV(address string) (k *KV, err error) {
 	conf := &api.Config{
 		Address: address,
-		Scheme: "http",
+		Scheme:  "http",
 	}
 	var c *api.Client
 	if c, err = api.NewClient(conf); err != nil {
-          return
+		return
 	}
 
 	k = &KV{
-		consulClient: c,
+		consulKV: c.KV(),
 	}
 	return
 }
